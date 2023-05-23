1fb548f5b5fc14ba50c373d4d018fdd98d3913bf
Chris Girard <chrisegirard@gmail.com>
2023-05-23 11:47:14 -0400

    Added A ton of classes and models for practice as well as actual project Models like User and Post will be commiting more frequently from here on out as well as creating a seperate project to contain my Test files that I filled with notes to help me learn.

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,11 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name = "Christopher";
+        int age = 2023 - 1998;
+        double price = 19.99;
+        boolean truth = true;
+        boolean falseness = !truth;
+    }
+}
---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,21 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 5;
+        double b = 2.5;
+        String c = "Chris";
+
+        // Calculated Variable Sum is a double
+        double sum = a + b;
+
+        // Calculated Variable concat is a String
+        String concat = a + c;
+
+        // Value of sum is 7.5;
+        System.out.println(sum);
+
+        // value of concat is "5Chris"
+        System.out.println(concat);
+    }
+}
